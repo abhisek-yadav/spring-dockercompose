7eb2996d254830c4724bdcb794009adb17b0fb42
Abhisek Yadav <abhisek.yadav@sap.com>
2020-07-23 22:47:24 +0530

    restart, env variables test

--- src/main/java/com/test/controller/VisitCountController.java
+++ src/main/java/com/test/controller/VisitCountController.java
@@ -7,24 +7,32 @@ import redis.clients.jedis.Jedis;
 @RestController
 public class VisitCountController {
 
-    @GetMapping("/ping")
-    public String ping() {
-        System.err.println("ping call");
-        return "Hello World!";
-    }
+  @GetMapping("/ping")
+  public String ping() {
+    System.err.println("ping call");
+    return "Hello World!";
+  }
 
-    @GetMapping("/visits")
-    public String visitCount() {
-        Jedis jedis = new Jedis("redis");
+  @GetMapping("/visits")
+  public String visitCount() {
+    try (Jedis jedis = new Jedis(System.getenv("REDIS_HOST"))) {
 
-        String visits = jedis.get("visit");
-        if (visits != null) {
-            jedis.set("visit", String.valueOf(Integer.parseInt(visits) + 1));
-            return visits;
-        } else {
-            jedis.set("visit", String.valueOf(1));
-            return String.valueOf(0);
-        }
+      // to test the environment variables passed via docker-compose file
+      //  System.err.println(System.getenv("REDIS_HOST"));
+      // System.err.println(System.getenv("REDIS_PORT"));
+
+      // to test the restart policy
+      // System.exit(0);
+
+      String visits = jedis.get("visit");
+      if (visits != null) {
+        jedis.set("visit", String.valueOf(Integer.parseInt(visits) + 1));
+        return visits;
+      } else {
+        jedis.set("visit", String.valueOf(1));
+        return String.valueOf(0);
+      }
     }
+  }
 
 }
